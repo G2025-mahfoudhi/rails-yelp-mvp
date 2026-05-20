8481e2e3be3b30f9ce01315057641b45f67a4156
Mahfoudhi Ghzela <ghzelamahfoudhi@yahoo.fr>
2026-05-20 14:24:32 +0200

    Yelp MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Restaurant.create!(name: "Epicure", address: "75008 Paris", category: "french", phone_number: "0123456789")
+Restaurant.create!(name: "Sushi Shop", address: "75001 Paris", category: "japanese", phone_number: "0987654321")
+Restaurant.create!(name: "Trattoria", address: "75003 Paris", category: "italian", phone_number: "0111222333")
+Restaurant.create!(name: "Beijing Palace", address: "75013 Paris", category: "chinese", phone_number: "0444555666")
+Restaurant.create!(name: "Moules Frites", address: "75006 Paris", category: "belgian", phone_number: "0777888999")
